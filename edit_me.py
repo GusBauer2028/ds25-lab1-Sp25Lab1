5ab4cc023aadf79619245d1f80fe97a99f850424
Augustus Bauer <gusbauer330@gmail.com>
2025-01-22 11:01:55 -0600

    Made Sudo code into python runable code

--- edit_me.py
+++ edit_me.py
@@ -1,12 +1,19 @@
 
 def find_max(lst):
     """return the maximum element"""
-
+#Author G. Bauer 1/22/2025
     current_max = lst[0]
 
-    #
-    #  YOUR CODE GOES HERE
-    #
+    
+    for i in range(1,len(lst)):
+        if lst[i]> current_max:
+            current_max = lst[i]
+            
+
+        
+    
+
+            return current_max
 
     return current_max
 
